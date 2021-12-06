ef1ec941b8127c5d89a52f533d28e6e71cdfd10f
Adam Azarchs <azarchs@lookfar.net>
2021-12-05 22:14:44 -0800

    Publish profiles, fix nullables.

--- PhotoTaggerTests/GpsLocationTests.cs
+++ PhotoTaggerTests/GpsLocationTests.cs
@@ -6,50 +6,49 @@ namespace PhotoTagger.Tests {
     public class GpsLocationTests {
         [TestMethod()]
         public void TryParseTest() {
-            GpsLocation result;
-            Assert.IsTrue(GpsLocation.TryParse("10.2,45.238", out result));
-            Assert.AreEqual("10.2, 45.238", result.ToString());
+            Assert.IsTrue(GpsLocation.TryParse("10.2,45.238", out GpsLocation? result));
+            Assert.AreEqual("10.2, 45.238", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("10.2, 45.016666666", out result));
-            Assert.AreEqual("10.2, 45.016666666", result.ToString());
+            Assert.AreEqual("10.2, 45.016666666", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("10.2N,45.238", out result));
-            Assert.AreEqual("10.2, 45.238", result.ToString());
+            Assert.AreEqual("10.2, 45.238", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("10.2,45.238fE", out result));
-            Assert.AreEqual("10.2, 45.238", result.ToString());
+            Assert.AreEqual("10.2, 45.238", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("10.256456456894N,45.238E", out result));
-            Assert.AreEqual("10.256456456894, 45.238", result.ToString());
+            Assert.AreEqual("10.256456456894, 45.238", result?.ToString());
 
             Assert.IsTrue(GpsLocation.TryParse("-10.2,45.238", out result));
-            Assert.AreEqual("-10.2, 45.238", result.ToString());
+            Assert.AreEqual("-10.2, 45.238", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("-10.2, 45", out result));
-            Assert.AreEqual("-10.2, 45", result.ToString());
+            Assert.AreEqual("-10.2, 45", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("10.2S,45.0166666667", out result));
-            Assert.AreEqual("-10.2, 45.0166666667", result.ToString());
+            Assert.AreEqual("-10.2, 45.0166666667", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("-10.2,45.238E", out result));
-            Assert.AreEqual("-10.2, 45.238", result.ToString());
+            Assert.AreEqual("-10.2, 45.238", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("10.2S,45.238E", out result));
-            Assert.AreEqual("-10.2, 45.238", result.ToString());
+            Assert.AreEqual("-10.2, 45.238", result?.ToString());
 
             Assert.IsTrue(GpsLocation.TryParse("10.2,-45.238", out result));
-            Assert.AreEqual("10.2, -45.238", result.ToString());
+            Assert.AreEqual("10.2, -45.238", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("10.2, -45.238", out result));
-            Assert.AreEqual("10.2, -45.238", result.ToString());
+            Assert.AreEqual("10.2, -45.238", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("10.2N,-45.238", out result));
-            Assert.AreEqual("10.2, -45.238", result.ToString());
+            Assert.AreEqual("10.2, -45.238", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("10.2,45.238W", out result));
-            Assert.AreEqual("10.2, -45.238", result.ToString());
+            Assert.AreEqual("10.2, -45.238", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("10.2N,45.238W", out result));
-            Assert.AreEqual("10.2, -45.238", result.ToString());
+            Assert.AreEqual("10.2, -45.238", result?.ToString());
 
             Assert.IsTrue(GpsLocation.TryParse("-10.2,-45.238", out result));
-            Assert.AreEqual("-10.2, -45.238", result.ToString());
+            Assert.AreEqual("-10.2, -45.238", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("-10.2, -45.238", out result));
-            Assert.AreEqual("-10.2, -45.238", result.ToString());
+            Assert.AreEqual("-10.2, -45.238", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("10.2S,-45.238", out result));
-            Assert.AreEqual("-10.2, -45.238", result.ToString());
+            Assert.AreEqual("-10.2, -45.238", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("-10.2,45.238W", out result));
-            Assert.AreEqual("-10.2, -45.238", result.ToString());
+            Assert.AreEqual("-10.2, -45.238", result?.ToString());
             Assert.IsTrue(GpsLocation.TryParse("10.2S,45.238W", out result));
-            Assert.AreEqual("-10.2, -45.238", result.ToString());
+            Assert.AreEqual("-10.2, -45.238", result?.ToString());
         }
     }
 }
